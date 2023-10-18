a1f35d82fccd481361b7c3260b3b6410a5a6f925
xialingfeng <lingfengxia.jake@gmail.com>
2023-10-18 10:57:51 -0400

    A3

--- src/Kanbas/Database/index.js
+++ src/Kanbas/Database/index.js
@@ -4,12 +4,6 @@ import assignments from "./assignments.json";
 import users from "./users.json";
 import enrollments from "./enrollments.json";
 import grades from "./grades.json";
-const courses = courses;
-const modules = modules;
-const assignments = assignments;
-const users = users;
-const enrollments = enrollments;
-const grades = grades;
 export default {
   courses,
   modules,
