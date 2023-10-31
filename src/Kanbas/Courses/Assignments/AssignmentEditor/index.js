a3ceee69cbbde56cd57b1b0472c2615ec88626a3
xialingfeng <lingfengxia.jake@gmail.com>
2023-10-31 17:39:29 -0400

    A4

--- src/Kanbas/Courses/Assignments/AssignmentEditor/index.js
+++ src/Kanbas/Courses/Assignments/AssignmentEditor/index.js
@@ -9,7 +9,7 @@ import {
 } from "../assignmentsReducer";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faCircleCheck, faEllipsisV} from "@fortawesome/free-solid-svg-icons";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 function AssignmentEditor() {
   const dispatch = useDispatch();
   const { assignmentId } = useParams();
