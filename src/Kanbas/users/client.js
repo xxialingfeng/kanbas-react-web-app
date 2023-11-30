b067490f1de800f9fef9b134530ba89bf0945db0
xialingfeng <lingfengxia.jake@gmail.com>
2023-11-30 15:10:41 -0500

    replace server link

--- src/Kanbas/users/client.js
+++ src/Kanbas/users/client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const request = axios.create({
   withCredentials: true,
 });
-export const BASE_API = 'http://localhost:4000';
+export const BASE_API = 'https://a6-node-server.onrender.com';
 export const USERS_API = `${BASE_API}/api/users`;
 export const signin = async (credentials) => {
   const response = await request.post(`${USERS_API}/signin`, credentials);
